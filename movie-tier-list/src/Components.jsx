617ab9eb807b20e5ab745ddb18b4bb9a5498a196
rjablon03 <secrets_crasser.0l@icloud.com>
2025-03-15 21:47:07 -0400

    Renamed for more specificity and did more styling

---
+++ movie-tier-list/src/Components.jsx
@@ -0,0 +1,34 @@
+const data = [
+    {
+        name: ""
+    }
+]
+
+function Container() {
+    return (
+        <>
+            <div className="container">
+                <Category label="S"/>
+                <Category label="A"/>
+                <Category label="B"/>
+                <Category label="C"/>
+                <Category label="D"/>
+                <Category label="E"/>
+            </div>
+        </>
+    )
+}
+
+// Category container so that the label is it's own thing and then to the right of it is the container for the items???
+function Category(props) {
+    return (
+        <div className="category-container">
+            <div className={"label label-" + props.label}>{props.label}</div>
+            <div className="items">
+
+            </div>
+        </div>
+    )
+}
+
+export { Container }
