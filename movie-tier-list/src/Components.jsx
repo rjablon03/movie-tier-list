3f853b1157e552b0dce744bb605c47ef1da7c3a4
rjablon03 <secrets_crasser.0l@icloud.com>
2025-03-15 23:10:07 -0400

    Put placeholder data in an array

--- movie-tier-list/src/Components.jsx
+++ movie-tier-list/src/Components.jsx
@@ -1,6 +1,45 @@
 const data = [
     {
-        name: "Interstellar"
+        name: "Interstellar",
+        director: "Christopher Nolan",
+        year: "2014",
+        rank: "S"
+    },
+    {
+        name: "La La Land",
+        director: "Damien Chazelle",
+        year: "2016",
+        rank: "S"
+    },
+    {
+        name: "The Sound of Music",
+        director: "Robert Wise",
+        year: "1965",
+        rank: "A"
+    },
+    {
+        name: "The Marksman",
+        director: "Robert Lorenz",
+        year: "2021",
+        rank: "B"
+    },
+    {
+        name: "28 Days Later",
+        director: "Danny Boyle",
+        year: "2002",
+        rank: "C"
+    },
+    {
+        name: "Eternals",
+        director: "Chloé Zhao",
+        year: "2021",
+        rank: "D"
+    },
+    {
+        name: "Green Lantern",
+        director: "Martin Campbell",
+        year: "2011",
+        rank: "E"
     }
 ]
 
@@ -19,7 +58,6 @@ function Container() {
     )
 }
 
-// Category container so that the label is it's own thing and then to the right of it is the container for the items???
 function Category(props) {
     return (
         <div className="category-container">
